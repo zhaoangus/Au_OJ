7411923ae081a57254c026868c6b775e5c7c591b
zhaoangus <zhao_angus@163.com>
2019-01-28 17:18:04 +0800

    createnews v1

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,23 +1,16 @@
 <template>
   <div class="news">
     <div class="overview-wrapper">
-      <div class="content">
+      <div class="tab">
         Overview
       </div>
     </div>
     <div class="content-wrapper">
       <div v-if="news" class="title">
-        <!-- 2018年ACM实验室招新个人赛 -->
         {{news.title}}
       </div>
       <div class="date">2018-10-09 20:37:29</div>
-      <div v-if="news" class="content">
-        <!-- 10月13日 周六 13:30 ~ 16:30
-        在塞北305 面向新老生开展一次招新赛
-        题目已经由小黑mua出好了，大家踊跃参加哦~
-
-        2018ACM招新报名群：905458438 -->
-        {{news.content}}
+      <div v-if="news" class="content" v-html="news.content">
       </div>
     </div>
   </div>
@@ -36,6 +29,8 @@ export default {
       id: 0,
       pageNews: [],
       currentNews: {}
+      // content: `<h1>111</h1>
+      // <br><h2 style="color: red;font-size: 24px">222</h2><br><button>btn</button>`
     }
   },
   computed: {
@@ -59,12 +54,12 @@ export default {
   .overview-wrapper
     margin: 30px 40px
     border-bottom: 1px solid rgba(7, 17, 27, .1)
-    .content
+    .tab
       display: inline-block
       padding: 10px 10px
       border-bottom: 1px solid $themeColor
-      font-size: 14px
       color: $themeColor
+      font-size: 14px
       cursor: pointer
   .content-wrapper
     margin: 30px 40px
@@ -79,5 +74,4 @@ export default {
       border-bottom: 1px solid rgba(7, 17, 27, .1)
     .content
       padding: 15px 0
-      font-size: 14px
 </style>
